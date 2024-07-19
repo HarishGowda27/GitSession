d3beaa564592d2f2a47c1e77601b7df14e155eab
Harish Gowda <harishgowda@Harishs-MacBook-Air.local>
2024-07-19 11:52:05 +0530

    added files

---
+++ src/com/pages/TermsConditions.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class TermsConditions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("T & C page");
+
+	}
+
+}
---
+++ src/com/test/SnippetSample.java
@@ -0,0 +1,11 @@
+package com.test;
+
+public class SnippetSample {
+	
+	public static void main(String args[])
+	{
+		String st = "Sample snippet";
+		System.out.println(st);
+	}
+}
+
