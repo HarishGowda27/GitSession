f65b10e7bfa3383de7ef6e4424929d83ab1736eb
kmharishgowda <27kmharishgowda@gmail.com>
2024-07-30 12:45:10 +0530

    search added

---
+++ src/com/pages/Searchpage.java
@@ -0,0 +1,11 @@
+package com.pages;
+
+public class Supportpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Support page");
+
+	}
+
+}
---
+++ src/com/test/SearchLogic.java
@@ -0,0 +1,15 @@
+package com.test;
+
+public class Testpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello searchpage");
+		
+		int a =1;
+		int b = 2;
+		System.out.print(a+b);
+
+	}
+
+}
