5db36c7653ca8a6aa8745d0adb2a018e6319fda7
kmharishgowda <27kmharishgowda@gmail.com>
2024-07-30 14:57:34 +0530

    search chnages latest

---
+++ src/com/pages/abc.java
@@ -0,0 +1,20 @@
+package com.pages;
+
+public class abc{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("T & C page");
+		
+		String str = "abc";
+		System.out.println(str);
+		
+		int abc = 1+1;
+		System.out.println(abc);
+		
+		int ab = 1+3;
+		System.out.println(ab);
+
+	}
+
+}
