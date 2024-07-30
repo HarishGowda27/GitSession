e44ec4f6a1357cc1d1149d9d75e448cab7c2ea0f
kmharishgowda <27kmharishgowda@gmail.com>
2024-07-30 14:53:30 +0530

    search chabges

--- src/com/pages/Supportpage.java
+++ src/com/pages/Supportpage.java
@@ -5,6 +5,7 @@ public class Supportpage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Support page");
+		System.out.println("Support page");
 
 	}
 
